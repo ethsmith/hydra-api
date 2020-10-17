987d59c92045e58d2067c4b2a0f9b8aa2e234c1e
ethsmith <ethan.smith1845@gmail.com>
2020-10-17 16:28:01 -0500

    Update play size annotations

--- src/main/java/org/dragonetmc/hydra/team/Teams.java
+++ src/main/java/org/dragonetmc/hydra/team/Teams.java
@@ -1,8 +1,13 @@
 package org.dragonetmc.hydra.team;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Teams {
-
     int min();
-
     int max();
 }
